f59e53b83c1f64ff32927997538ec4b131e54c8b
cyberpunkcoder <cyberpunkcoder@gmail.com>
2020-10-29 18:14:56 -0700

    first commit

---
+++ start-coding.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Author: cyberpunkprogrammer (github.com/cyberpunkprogrammer) (cyberpunkprogrammer@gmail.com)
+# Date: October 29, 2020
+# Bash script to bring up your IDE and randomly shuffle your music into a play queue
+
+# TODO: change this line to your coding music directory
+DIR=~/Music/Coding/
+
+IFS=$'\n'
+SONGS="$(ls $DIR | shuf)"
+
+# TODO: change this line to your preferred IDE
+code
+
+rhythmbox-client --pause
+rhythmbox-client --clear-queue
+
+# automatically shuffle playlist
+for song in $SONGS; do
+	rhythmbox-client --enqueue "$DIR$song"
+done
+
+rhythmbox-client --next
+rhythmbox-client --play
